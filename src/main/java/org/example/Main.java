cf737225dea75905b03afe78abb1ae0bf083987d
Yaroslav-Korzh <dunny04@gmail.com>
2023-12-21 11:32:01 +0100

    New task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        int[] array = {2, 7, 11, 15, 2, 7};
+        int target = 9;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < array.length; j++) {
+                if (array[i] + array[j] == target) {
+                    System.out.println(i + " " + j);
+                array[i] += 100;
+                array[j] += 100;
+                }
+            }
+            i++;
+        }
     }
 }
